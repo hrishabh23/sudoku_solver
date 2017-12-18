aaf4cc9f4481410b1ab00311e7f6cf532874bade
Hrishabh <rajputhrishabh@gmail.com>
2017-12-19 04:46:38 +0530

    made code more robust

--- src/lib/bruteforce.cpp
+++ src/lib/bruteforce.cpp
@@ -4,44 +4,64 @@
 
 #define INDEX 9
 
-void get_input(int grid[][INDEX])
+bool get_input(int grid[][INDEX])
 {
 	int row[INDEX], row_index = 0, column_index = 0, select=0;
 	std::cout<<"Press 1 to Enter the Sudoku puzzle manually:"<<std::endl;
 	std::cout<<"Press 2 to Enter the Sudoku puzzle stored in .txt file"<<std::endl;
 	std::cin>>select;
+	std::cin.ignore(32767, '\n');
 	
 	switch(select)
 	{
-		case 2:
+					
+		case 1:
 		{
+			std::cout<<"You can enter your puzzle. Enter 0 when empty:"<<std::endl;
+			for(row_index = 0 ; row_index < INDEX ; row_index++)
+			{
+				std::cin >> row[row_index];
+				std::cin.ignore(32767, '\n');
+			}
+			break;
+		}
+
+		case 2:
+		{	std::cout<<"Reading the file..."<<std::endl;
 			std::ifstream puzzle_file;
 			puzzle_file.open("puzzle.txt");
 			
+			if(puzzle_file.fail())
+			{
+				std::cout<<"Error! File not found."<<std::endl;
+				return false;
+			}
+			
+			std::cout<<"Done."<<std::endl;
 			for(row_index=0 ; row_index < INDEX ; row_index++)
 			{
 				puzzle_file>>row[row_index];
 			}		
 			puzzle_file.close();
-		}
-			
-		case 1:
-			if(select==1)
-				for(row_index = 0 ; row_index < INDEX ; row_index++)
-				{
-					std::cin >> row[row_index];
-				}
-	
-			for(row_index = 0 ; row_index < INDEX ; row_index++)
-			{
-				for(column_index = INDEX-1 ; column_index >= 0 ; column_index--)
-				{
-					grid[row_index][column_index] = row[row_index]%10;
-					row[row_index] /= 10;
-				}
-			}
 			break;
+		}
+		
+		default:
+		{
+			std::cout<<"Error! Invalid Option Chosen. Please Choose from the Options."<<std::endl;
+			return false;
+		}
 	}
+	
+	for(row_index = 0 ; row_index < INDEX ; row_index++)
+	{
+		for(column_index = INDEX-1 ; column_index >= 0 ; column_index--)
+		{
+			grid[row_index][column_index] = row[row_index]%10;
+			row[row_index] /= 10;
+		}
+	}
+	return true;
 }
 
 void assign_initial(short &row, short &column, int grid[][INDEX])
@@ -59,18 +79,20 @@ void assign_initial(short &row, short &column, int grid[][INDEX])
 		}
 }
 
-void go_to_previous(short &row, short &column, struct blank_unit_list** current_node)
+bool go_to_previous(short &row, short &column, struct blank_unit_list** current_node)
 {
-	//std::cout<<"Went previous"<<std::endl;
-	//struct blank_unit_list* temp = current_node;
+	if((*current_node)->previous==NULL)
+	{
+		std::cout<<"Error! Something is wrong in puzzle. It does not have any solution."<<std::endl;
+		return false;
+	}
 	(*current_node) = (*current_node)->previous;
-	//delete temp;
 	row = (*current_node)->row;
 	column = (*current_node)->column;
-	//std::cout<<row<<", "<<column<<std::endl;
+	return true;
 }
 
-bool is_valid(short &row, short &column, int grid[][INDEX])
+bool is_valid(short row, short column, int grid[][INDEX])
 {
 	short row_index, column_index;
 	short block_row_index = 3*(row/3), block_column_index = 3*(column/3);
@@ -97,23 +119,21 @@ bool is_valid(short &row, short &column, int grid[][INDEX])
 	return true;
 }
 
-bool not_solved(int grid[][INDEX])
+bool solved(int grid[][INDEX], short row, short column)
 {
 	int row_index, column_index;
-	short eight=8;
 	
 	for(row_index = 0 ; row_index < INDEX ; row_index++)
 	{
 		for(column_index = 0 ; column_index < INDEX ; column_index++)
 		{
-			if(!grid[row_index][column_index]) return true;
+			if(!grid[row_index][column_index]) return false;
 		}
 	}
-	if(is_valid(eight, eight, grid))
-	return false;
+	if(is_valid(row, column, grid))
 	return true;
+	return false;
 }
-
 
 void go_to_next(short &row, short &column, int grid[][INDEX], struct blank_unit_list** current_node)
 {
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<cstdio>
 #include"bruteforce.h"
 #include"series.h"
 
@@ -7,34 +8,53 @@
 int main()
 {
 	int grid[INDEX][INDEX];
-	short x, y;
+	short x, y, flag=0;
 	struct blank_unit_list *current_node;
-	get_input(grid);
+	while(!get_input(grid));
 	assign_initial(x, y, grid);
 	current_node = new struct blank_unit_list;
 	current_node->row=x;
 	current_node->column=y;
 	current_node->previous=NULL;
 	
-	while(not_solved(grid))
+	while(1)
 	{
-		z=0;
 		if(grid[x][y]==9)
 		{
 			grid[x][y]=0;
-			go_to_previous(x, y, &current_node);
+			if(!go_to_previous(x, y, &current_node))
+			{
+				break;
+			}
 			continue;
 		}
-			grid[x][y]++;
+		
+		grid[x][y]++;
+				
+		if(solved(grid, x, y))
+		{
+			if(flag>10)
+			{
+				std::cout<<"There are more than 10 Solutions for this puzzles. Hence, the process is going to be stopped!"<<std::endl;
+				return 0;
+			}
+			if(flag==1)
+			{
+				std::cout<<"There are multiple solutions for the puzzle."<<std::endl;
+				std::cout<<"Press <return> to continue...";
+				getchar();
+			}
+			print_grid(grid);
+			flag++;
+			
+		}
+
 			
 		if(is_valid(x, y, grid))
 		{
 			go_to_next(x, y, grid, &current_node);
 		
 		}
-		
 	}
-	
-	print_grid(grid);
 	return 0;
 }
